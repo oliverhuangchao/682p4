305bba9626e5b7b2e8c7b9527b9e31e2bcac6003
Chao Huang <chaoh@glare8.cs.clemson.edu>
2014-09-21 20:56:17 -0400

    add a simple table view to the search section

---
+++ Viewtext/SearchBookViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchBookViewController.h
+//  Viewtext
+//
+//  Created by Chao Huang on 9/21/14.
+//  Copyright (c) 2014 Clemson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchBookViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
---
+++ Viewtext/SearchBookViewController.m
@@ -0,0 +1,126 @@
+//
+//  SearchBookViewController.m
+//  Viewtext
+//
+//  Created by Chao Huang on 9/21/14.
+//  Copyright (c) 2014 Clemson. All rights reserved.
+//
+
+#import "SearchBookViewController.h"
+#import "GetMethodsConnect.h"
+
+@interface SearchBookViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *searchBookNameTextField;
+
+
+@property (nonatomic) NSString *searchBookName;
+@property (nonatomic) NSArray *bookListArray;
+@end
+
+
+
+@implementation SearchBookViewController
+
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.bookListArray = [NSArray arrayWithObjects:@"hello",@"world", nil];
+    [self setUpForDismissKeyboard];
+
+}
+
+- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.bookListArray count];
+}
+
+
+- (UITableView *)tableView:(UITableView *) tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"simpleTableCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    cell.textLabel.text = [self.bookListArray objectAtIndex:indexPath.row];
+    cell.imageView.image = [UIImage imageNamed:@"garfield"];
+    return cell;
+}
+
+
+- (IBAction)searchBookNameClearName:(UITextField *)sender {
+    self.searchBookNameTextField.text = @"";
+}
+
+- (IBAction)searchBookNameGetResult:(UITextField *)sender {
+    self.searchBookName = self.searchBookNameTextField.text;
+    NSString *basic_URL = [NSString stringWithFormat:@"http://people.cs.clemson.edu/~chaoh/ios/searchBook.php?bookName=%@",self.searchBookName];
+    NSData *resultData = [GetMethodsConnect getContentFromPhp:basic_URL];
+    NSString *resultString = [[NSString alloc] initWithData:resultData encoding:NSUTF8StringEncoding];
+    NSLog(@"%@",resultString);
+}
+
+
+
+
+
+
+
+
+
+
+- (void) setUpForDismissKeyboard{
+    NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
+    UITapGestureRecognizer *singleTapGR = [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                                                  action:@selector(tapAnywhereToDismissKeyboard:)];
+    NSOperationQueue *mainQueue = [NSOperationQueue mainQueue];
+    [nc addObserverForName:UIKeyboardWillShowNotification
+                    object:nil
+                     queue:mainQueue
+                usingBlock:^(NSNotification *note){
+                    [self.view addGestureRecognizer:singleTapGR];
+                }];
+    [nc addObserverForName:UIKeyboardWillHideNotification
+                    object:nil
+                     queue:mainQueue
+                usingBlock:^(NSNotification *note){
+                    [self.view removeGestureRecognizer:singleTapGR];
+                }];
+}
+
+-(void) tapAnywhereToDismissKeyboard:(UIGestureRecognizer *)gestureRecognizer{
+    [self.view endEditing:YES];
+}
+
+
+/*
+//pragma mark - Navigation
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+ 
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+if (self) {
+// Custom initialization
+}
+return self;
+}
+ 
+- (void)didReceiveMemoryWarning
+{
+[super didReceiveMemoryWarning];
+// Dispose of any resources that can be recreated.
+}
+
+ 
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
